669b1c6ce6d326faa1794fdddeb1bf542b4e0b31
Coy <cmhumphr@ucsc.edu>
2015-03-01 18:56:59 -0800

    Set migrate to false in scheduler.py

--- models/scheduler.py
+++ models/scheduler.py
@@ -3,4 +3,4 @@ def task_send (to, subject, message):
     mail.send (to = to, subject = subject, message = message)
 
 from gluon.scheduler import Scheduler
-scheduler = Scheduler(db)
+scheduler = Scheduler(db, migrate=False)
