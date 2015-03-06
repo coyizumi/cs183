5bccfda6d6a20c3ccb457b037a2bb5fb20474c51
Coy <cmhumphr@ucsc.edu>
2015-03-05 22:48:31 -0800

    Added code for comments to Dustins code

--- controllers/default.py
+++ controllers/default.py
@@ -135,12 +135,6 @@ def search():
 
     return dict(form=form)
 
-def view_post():
-    post_id = request.args[0] or None
-    post = db.posting[post_id]
-    comments = db(db.comments.post == post).select (orderby=~db.revision.date_posted)
-    return dict (post=post, comments=comments)
-
 
 def user():
     """
