c0dcb7a63ebcd60ac86d19cbfc784eef4056a9bb
Coy <cmhumphr@ucsc.edu>
2015-03-16 20:18:10 -0700

    Fixed bug where washington dc was not searchable

--- models/tables.py
+++ models/tables.py
@@ -9,7 +9,7 @@ STATES = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado',
 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 
 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 
 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
-'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 
+'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'Washington D.C.', 'West Virginia', 
 'Wisconsin', 'Wyoming']
 
 RATINGS = ['Great', 'Meh', 'Bad']
